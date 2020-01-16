04762abeb7f5f2aaf164601c3ef9b73f973220f4
Ahmed Hosny <i.ahmedhosny@gmail.com>
2020-01-16 12:54:37 -0500

    iter

--- files/run.py
+++ files/run.py
@@ -1,13 +1,44 @@
 import json
 from model import Xnet
 from keras.models import model_from_json
+from data import get_data
+import tensorflow as tf
 
-model = Xnet(decoder_block_type='upsampling')
-model.summary()
 
-model_json = model.to_json()
-with open("original_model_upsample.json", "w") as json_file:
-    json_file.write(model_json)
+# from keras import backend as K
+# # tf.logging.set_verbosity(tf.logging.ERROR)
+# config = tf.ConfigProto()
+# config.gpu_options.allow_growth = True
+
+
+# data
+data = get_data()
+
+# model
+model = Xnet(input_shape=(64, 64, 3), decoder_block_type='upsampling')
+# model.summary()
+model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['binary_accuracy'])
+
+# with tf.device('/gpu:0'):
+model.fit(x=data["train"]["images"], y=data["train"]["labels"], batch_size=2, epochs=10)
+
+
+
+
+#
+# predictions = model.predict_on_batch(data["test"]["images"])
+# print(predictions.shape)
+# print(predictions[0])
+#
+# predictions = model.predict(data["test"]["images"])
+# print(predictions.shape)
+# print(predictions[0])
+
+
+# model_json = model.to_json()
+# with open("original_model_upsample.json", "w") as json_file:
+#     json_file.write(model_json)
+
 
 # json_file = open('original_model_upsample.json', 'r')
 # original_model_json = json_file.read()
