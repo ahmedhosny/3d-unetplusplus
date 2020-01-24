9eaaa41031bd3f79e8e4a5203237d6546a0fa805
Ahmed Hosny <i.ahmedhosny@gmail.com>
2020-01-24 16:06:17 -0500

    adds custom loss and metrics file

---
+++ files/custom_funcs.py
@@ -0,0 +1,19 @@
+from keras import backend as K
+
+# not tested
+def iou_coef(y_true, y_pred, smooth=1):
+  intersection = K.sum(K.abs(y_true * y_pred), axis=[1,2,3])
+  union = K.sum(y_true,[1,2,3])+K.sum(y_pred,[1,2,3])-intersection
+  iou = K.mean((intersection + smooth) / (union + smooth), axis=0)
+  return iou
+
+
+def dice_coefficient(y_true, y_pred, smooth=1.):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coefficient_loss(y_true, y_pred):
+    return -dice_coefficient(y_true, y_pred)
