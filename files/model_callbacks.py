ffb17e4f8ab60bc18a92e01ff4108373f1dd2353
Ahmed Hosny <i.ahmedhosny@gmail.com>
2020-01-31 15:09:33 -0500

    saves losses to numpy at end of training

--- files/model_callbacks.py
+++ files/model_callbacks.py
@@ -1,4 +1,5 @@
 import keras
+import numpy as np
 
 class Cbk(keras.callbacks.Callback):
 
@@ -6,15 +7,17 @@ class Cbk(keras.callbacks.Callback):
         self.model_to_save = model
         self.label = label
         self.run = RUN
-
-    def on_train_begin(self, logs={}):
         self.losses = []
         self.val_losses = []
 
     def on_batch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
-        self.val_losses.append(logs.get('val_loss'))
-        # np.save("siko.npy", self.losses)
 
     def on_epoch_end(self, epoch, logs=None):
+        self.val_losses.append(logs.get('val_loss'))
+        # save model
         self.model_to_save.save('/output/{}_label_{}_epoch_{}_val-loss_{}.h5'.format(str(self.run), self.label, str(epoch), str(logs["val_loss"])))
+
+    def on_train_end(self, logs={}):
+        np.save('/output/{}_label_{}_loss.npy'.format(str(self.run), self.label), self.losses)
+        np.save('/output/{}_label_{}_val_loss.npy'.format(str(self.run), self.label), self.val_losses)
