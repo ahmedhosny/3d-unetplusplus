5d68b8c532b407db11734f5230069482aa4b7e4a
Ahmed Hosny <i.ahmedhosny@gmail.com>
2020-01-31 12:06:19 -0500

    extracts losses on batch end, but doesn't write them yet

--- files/model_callbacks.py
+++ files/model_callbacks.py
@@ -7,5 +7,14 @@ class Cbk(keras.callbacks.Callback):
         self.label = label
         self.run = RUN
 
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.val_losses = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
+        # np.save("siko.npy", self.losses)
+
     def on_epoch_end(self, epoch, logs=None):
         self.model_to_save.save('/output/{}_label_{}_epoch_{}_val-loss_{}.h5'.format(str(self.run), self.label, str(epoch), str(logs["val_loss"])))
