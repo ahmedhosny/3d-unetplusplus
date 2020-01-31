eb85bad2c732af4ce9d6bc1af9ad46be19dce7f4
Ahmed Hosny <i.ahmedhosny@gmail.com>
2020-01-31 17:49:05 -0500

    allows for saving logs and saving only the best model

--- files/model_callbacks.py
+++ files/model_callbacks.py
@@ -3,21 +3,26 @@ import numpy as np
 
 class Cbk(keras.callbacks.Callback):
 
-    def __init__(self, model, label, RUN):
+    def __init__(self, model, RUN):
         self.model_to_save = model
-        self.label = label
         self.run = RUN
         self.losses = []
         self.val_losses = []
+        self.best_val_loss = 0
 
     def on_batch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
 
     def on_epoch_end(self, epoch, logs=None):
-        self.val_losses.append(logs.get('val_loss'))
+        val_loss = logs.get('val_loss')
+        self.val_losses.append(val_loss)
         # save model
-        self.model_to_save.save('/output/{}_label_{}_epoch_{}_val-loss_{}.h5'.format(str(self.run), self.label, str(epoch), str(logs["val_loss"])))
+        if val_loss < self.best_val_loss:
+            self.model_to_save.save('/output/{}_epoch_{}_val-loss_{}.h5'.format(self.run, epoch, val_loss))
+            print('model saved.')
+            self.best_val_loss = val_loss
 
-    def on_train_end(self, logs={}):
-        np.save('/output/{}_label_{}_loss.npy'.format(str(self.run), self.label), self.losses)
-        np.save('/output/{}_label_{}_val_loss.npy'.format(str(self.run), self.label), self.val_losses)
+        # save logs (overwrite)
+        np.save('/output/{}_loss.npy'.format(self.run), self.losses)
+        np.save('/output/{}_val_loss.npy'.format(self.run), self.val_losses)
+        print('log saved.')
