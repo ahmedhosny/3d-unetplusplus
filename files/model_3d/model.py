79669be80ec301e16986b4c514821e56e84c0448
Ahmed Hosny <i.ahmedhosny@gmail.com>
2020-02-14 14:34:00 -0500

    model is compiled in run.py not in model.py

--- files/model_3d/model.py
+++ files/model_3d/model.py
@@ -117,5 +117,5 @@ def isensee2017_model(input_shape=(4, 128, 128, 128), n_base_filters=16, depth=5
     activation_block = Activation(activation_name)(output_layer)
 
     model = Model(inputs=inputs, outputs=activation_block)
-    model.compile(optimizer=optimizer(lr=initial_learning_rate), loss=loss_function)
+    # model.compile(optimizer=optimizer(lr=initial_learning_rate), loss=loss_function)
     return model
