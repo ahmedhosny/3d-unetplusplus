b01f3708bdf02d63b8f82f1f881e0ddd5c49dc0e
Ahmed Hosny <i.ahmedhosny@gmail.com>
2020-03-03 12:32:46 -0500

    start script updates

--- start/start.sh
+++ start/start.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-## user set variables
-# the aws instance IP - user is ubuntu
-aws_ip="ec2-18-234-115-94.compute-1.amazonaws.com"
-# path to ssh key
-path_to_key="/home/hosny/Dropbox/DFCI/00_access/aws-segmentation/aws-segmentation.pem"
-# working within the instance
-working_dir="cd /ahmed/code/3d-unetplusplus"
-screen_docker="screen -S docker-screen docker run -it --gpus all -v /ahmed/code/files:/files -v /ahmed/output:/output -v /ahmed/data:/data dkr-model /bin/bash"
-# working locally
-local_code_folder="/home/hosny/Dropbox/DFCI/30_prospective_validation/04_model/3d-unetplusplus"
+# figure our remote ssh
+# install nvidia drivers
+# install docker
+# install docker nvidia
+# mount external drives
+# install screen
+
 
 ## script variables
-ssh_connect="ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $path_to_key ubuntu@$aws_ip"
+path_to_key="-i /home/hosny/Dropbox/DFCI/00_access/R2-X2/R2-X2"
+port="-p 1025"
+user_ip="hosny@172.24.189.56"
+
+ssh_connect="ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $path_to_key $port $user_ip"
 nvidia_watch="watch -n 0.5 nvidia-smi"
 
-## terminal 1 - monitor aws gpus
+screen_docker="screen -S docker-screen sudo docker run -it --gpus all -v /home/hosny//segmentation/files:/files -v /home/hosny/segmentation/output:/output -v /mnt:/mnt dkr-model /bin/bash"
+
+local_folder="/home/hosny/Dropbox/DFCI/30_prospective_validation/04_model/3d-unetplusplus"
+
+# ## terminal 1 - monitor gpus
 terminal_1="$ssh_connect '$nvidia_watch;command;/bin/bash'"
 gnome-terminal -e "$terminal_1"
 
 ## terminal 2 - ssh, screen and docker
-terminal_2="$ssh_connect '$working_dir;$screen_docker;command;/bin/bash'"
+terminal_2="$ssh_connect '$screen_docker;command;/bin/bash'"
 gnome-terminal -e "$terminal_2"
 
-## terminal 3 - send code update to AWS instance
-gnome-terminal -e "$PWD/type_command.sh \"rsync --progress --stats --rsync-path='sudo rsync' -rave 'ssh -i $path_to_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' $local_code_folder/files ubuntu@$aws_ip:/ahmed/code\""
+## terminal 3 - send code update to instance
+gnome-terminal -e "$PWD/type_command.sh \"rsync --progress --stats -rave 'ssh $path_to_key $port -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' $local_folder/files/ $user_ip:/home/hosny/segmentation/files/\""
 
 ## terminal 4 - get output from AWs output folder
-gnome-terminal -e "$PWD/type_command.sh \"rsync --progress --stats --rsync-path='sudo rsync' -rave 'ssh -i $path_to_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' ubuntu@$aws_ip:/ahmed/output $local_code_folder\""
+gnome-terminal -e "$PWD/type_command.sh \"rsync --progress --stats -rave 'ssh $path_to_key $port -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' $user_ip:/home/hosny/segmentation/output/ $local_folder/output/\""
 
-# terminal 5 (same terminal) - open local jupyter notebook in local docker
-docker run -it --gpus all -v $local_code_folder/files:/files -v $local_code_folder/output:/output -v /mnt/aertslab/DATA/Lung/RTOG_0617/0_processed:/data -p 8889:8888 dkr-model jupyter notebook --allow-root --ip=0.0.0.0
+# run locally
+docker run -it -v $local_folder/files:/files -v $local_folder/output:/output -v /mnt:/mnt -p 8889:8888 dkr-model jupyter notebook --allow-root --ip=0.0.0.0
